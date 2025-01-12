541b753ed4d32a1351822e17e916ed6c07134c58
Vikas Tiwari <vikastiwari809028@gmail.com>
2025-01-12 11:52:41 +0530

    JWT Authentication added

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ import cloudinary from "cloudinary";
 import Razorpay from "razorpay";
 
 const PORT = process.env.PORT || 5000;
-// Cloudinary configuration
+
 cloudinary.v2.config({
   cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
   api_key: process.env.CLOUDINARY_API_KEY,
